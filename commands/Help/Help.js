2c62299bbc1549d422473fddc0a76d511d7586dd
Aviral Srivastava <79373546+CodyAvi@users.noreply.github.com>
2021-08-18 21:19:05 +0530

    Update Help.js

--- commands/Help/Help.js
+++ commands/Help/Help.js
@@ -6,26 +6,26 @@ module.exports =
     {
         p=prefix
         message.channel.send(`
-\`\`\`My Commands :
+***My Commands :***
         
- ${p}clear => clear n no of previous messages [creates a new channel to show logs]
+ \`${p}clear\` => clear n no of previous messages [creates a new channel to show logs]
 
- pref => To get to know ur prefix
+ \`pref\` => To get to know ur prefix
 
- ${p}clog {e/d}[enable/disable] => gives log of deleted messages if enabled
+ \`${p}clog {e/d}[enable/disable]\` => gives log of deleted messages if enabled
  
- ${p}set pref {reset / ur custom prefix} => to set custom prefix for separate servers
+ \`${p}set pref {reset / ur custom prefix}\` => to set custom prefix for separate servers
  
- ${p}crole {role name} => creates a role with default permissions
+ \`${p}crole {role name}\` => creates a role with default permissions
  
- ${p}role {role name} @{person} => assigns a specific role to a specific person
+ \`${p}role {role name} @{person}\` => assigns a specific role to a specific person
  
- ${p}rmr {role name} @{person} => removes a specific role from a specific person
+ \`${p}rmr {role name} @{person}\` => removes a specific role from a specific person
  
- ${p}rinfo {role name} => no use basically provide date of role created and it colour in base 10
+ \`${p}rinfo {role name}\` => no use basically provide date of role created and it colour in base 10
  
- ${p}help => to repeat this bullshit..
+ \`${p}help\` => to repeat this bullshit..
         
-Note : Commands are to be used in the way they are written\`\`\``)
+**Note :** *Commands are to be used the way they are written`)
     }
 }
